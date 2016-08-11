903a142f30ab603411fee9d74ff62cd3124f3f03
Hristo Tanev <hristotanev7@gmail.com>
2016-08-11 14:56:23 +0100

    Added a search field

--- src/js/components/Search.js
+++ src/js/components/Search.js
@@ -14,13 +14,27 @@ export default class Search extends React.Component {
 		});
 	}
 
+	findPlace(e) {
+		if (e.keyCode==13) {
+			e.preventDefault();
+			
+			const { query }=this.state;
+			console.log(query);
+
+			this.setState({
+				query: ''
+			});
+		}
+	}
+
 	render() {
 		const { query }=this.state;
 
 		return (
 			<div id="search">
 				<label>Find:&nbsp;</label>
-				<input type="text" onChange={this.getQuery.bind(this)} value={query} />
+				<input type="text" onChange={this.getQuery.bind(this)} value={query} 
+				onKeyDown={this.findPlace.bind(this)} />
 			</div>
 		)
 	}
