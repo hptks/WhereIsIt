0bbecc03736121e8dc7bad84cfa289b6c5a8b866
Hristo Tanev <hristotanev7@gmail.com>
2016-08-14 19:49:42 +0100

    Added an option to get users current location. The default location is central London.

--- src/js/pages/WhereIs.js
+++ src/js/pages/WhereIs.js
@@ -12,11 +12,45 @@ export default class WhereIs extends React.Component {
 		};
 	}
 
+	componentDidMount() {
+		let element=document.getElementById('supports');
+		const { lat, lng }=this.state;
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				this.setState({
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				});
+			}, () => {
+				const nlat=this.state.lat, nlng=this.state.lng;
+				if (lat==nlat&&lng==nlng) {
+					this.handleLocationError(false, element);
+				} else {
+					this.handleLocationError(true, element);
+				}
+			});
+		} else {
+			this.handleLocationError(false, element);
+		}
+	}
+
+	handleLocationError(browserSupportsLocation, element) {
+		if (browserSupportsLocation) {
+			element.innerHTML='';
+			element.innerHTML='Location found';
+		} else {
+			element.innerHTML='';
+			element.innerHTML='Location not found';
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				<Map lat={this.state.lat} lng={this.state.lng} />
-				<Search />
+				<Search /><br />
+				<div id="supports"></div>
 			</div>
 		)
 	}
